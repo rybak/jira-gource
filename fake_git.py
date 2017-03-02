8f7303aeb4380e68fe8ecbe66299f56bf68752e9
Andrei Rybak <rybak.a.v@gmail.com>
2017-03-02 22:01:56 +0300

    Fix bugs in creating commits
    +Add some return code checks

--- fake_git.py
+++ fake_git.py
@@ -6,17 +6,19 @@ import jira
 
 def create_commit(committer_name, committer_email, timestamp, commit_message):
     is_git_repo = is_current_dir_git_repo()
+    if os.path.basename(os.getcwd()) != "temp_repo":
+        raise Exception("Wrong repo")
     if not is_git_repo:
         print("Trying to create commit")
         print("Not a git repo")
         print("Bailing out")
         return -42
-    os.environ["GIT_COMMITTER"] = committer_name
-    os.environ["GIT_AUTHOR"] = committer_name
+    os.environ["GIT_COMMITTER_NAME"] = committer_name
+    os.environ["GIT_AUTHOR_NAME"] = committer_name
     os.environ["GIT_COMMITTER_EMAIL"] = committer_email
     os.environ["GIT_AUTHOR_EMAIL"] = committer_email
     os.environ["GIT_AUTHOR_DATE"] = str(timestamp)
-    os.environ["GIT_COMMIT_DATE"] = str(timestamp)
+    os.environ["GIT_COMMITTER_DATE"] = str(timestamp)
     return call(['git', 'commit', '-m', commit_message])
 
 
@@ -25,11 +27,10 @@ def is_current_dir_git_repo():
 
 
 def create_repo(path: str):
-    os.chdir(path)
     is_git_repo = is_current_dir_git_repo()
     if is_git_repo:
         print("Already a git repo")
-        return True
+        return False
     else:
         ret = call(['git', 'init'])
         if ret != 0:
@@ -40,11 +41,12 @@ def create_repo(path: str):
             return True
 
 
-repo_dir = 'temp_repo'
+repo_path = os.path.join(os.path.expanduser("~"), 'temp_repo')
+os.chdir(repo_path)
 
-if not create_repo(repo_dir):
-    print("Failed to create git repo")
-    exit(1)
+# if not create_repo(repo_dir):
+#     print("Failed to create git repo")
+#     exit(1)
 
 tickets_to_process = []
 for i in range(18000, 18010):
@@ -74,10 +76,13 @@ for tk in sorted(changes):
     iso_time = ISO.parse(timestamp)
     print("{k}: @{t}: {n} <{e}>".format(k=key, t=iso_time, n=name, e=email))
     with open(key, 'a') as f:
-        f.write("Change by {n} @ {t}".format(n=name, t=iso_time))
-    call(['git', 'add', key])
+        f.write("Change by {n} @ {t}\n".format(n=name, t=iso_time))
+    ret = call(['git', 'add', key])
+    if ret != 0:
+        print("add failed")
+        break
     ret = create_commit(name, email, iso_time, "change")
     if ret != 0:
-        print("failed")
+        print("commit failed")
         break
 
