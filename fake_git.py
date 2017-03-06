746238997f03a85e44d97b58dde7153d8ac194f6
Andrei Rybak <rybak.a.v@gmail.com>
2017-03-06 12:35:28 +0300

    fake_git.py: add KeyboardInterrupt handling
    Running the script takes a long time.  For testing purposes and for
    user's convenience, make fake_git handle Ctrl-C somewhat gracefully.

--- fake_git.py
+++ fake_git.py
@@ -68,39 +68,48 @@ for key in tickets_to_process:
         changes[timestamp + key] = h
 
 names = set()
-for tk in sorted(changes):
-    h = changes[tk]
-    key = h['ticket']
-    name = h['author']['displayName']
-    names.add(name)
-    email = h['author']['emailAddress']
-    timestamp = h['created']
-    iso_time = ISO.parse(timestamp)
-    print("{k}: @{t}: {n} <{e}>".format(k=key, t=iso_time, n=name, e=email))
-    with open(key, 'a') as f:
-        f.write("Change by {n} @ {t}\n".format(n=name, t=iso_time))
-    ret = call(['git', 'add', key])
-    if ret != 0:
-        print("add failed")
-        break
-    ret = create_commit(name, email, iso_time, "change {k}".format(k=key))
-    if ret != 0:
-        print("commit failed")
-        break
-    # check if `h` is the last change on the `key` ticket
-    last_change = jira.get_history(jira.tickets_json[key]['JIRA'])[-1]
-    if last_change['created'] == timestamp:
-        # if h is the last change - delete the ticket file and commit again
-        # h == last_change
-        ret = call(['git', 'rm', key])
+try:
+    for tk in sorted(changes):
+        h = changes[tk]
+        key = h['ticket']
+        name = h['author']['displayName']
+        names.add(name)
+        email = h['author']['emailAddress']
+        timestamp = h['created']
+        iso_time = ISO.parse(timestamp)
+        print("{k}: @{t}: {n} <{e}>".format(k=key, t=iso_time, n=name, e=email))
+        with open(key, 'a') as f:
+            f.write("Change by {n} @ {t}\n".format(n=name, t=iso_time))
+        ret = call(['git', 'add', key])
         if ret != 0:
-            print("rm failed")
+            print("add failed")
             break
-        ret = create_commit(name, email, iso_time, "last change {k}".format(k=key))
+        ret = create_commit(name, email, iso_time, "change {k}".format(k=key))
         if ret != 0:
             print("commit failed")
             break
 
+        # check if `h` is the last change on the `key` ticket
+        last_change = jira.get_history(jira.tickets_json[key]['JIRA'])[-1]
+        if last_change['created'] == timestamp:
+            # if h is the last change - delete the ticket file and commit again
+            # h == last_change
+            ret = call(['git', 'rm', key])
+            if ret != 0:
+                print("rm failed")
+                break
+            ret = create_commit(name, email, iso_time, "last change {k}".format(k=key))
+            if ret != 0:
+                print("commit failed")
+                break
+except KeyboardInterrupt:
+    print("Interrupted by user. Stopping...")
+except Exception as e:
+    print("Unexpected exception", e)
+    print("Bailing out")
+
+print("Saving names of committers")
+# append, to avoid any data loss. Just `sort -u names.txt` later.
 with open("names.txt", "a") as f:
     f.write("\n".join(names))
 
