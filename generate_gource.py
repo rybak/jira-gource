6912d26a0ad1559caaabcab0e39fd1ba3ba030c8
Andrei Rybak <rybak.a.v@gmail.com>
2017-07-23 17:11:51 +0300

    Add logging

--- generate_gource.py
+++ generate_gource.py
@@ -29,6 +29,7 @@ gource_input_txt = "gource-input-{0}.txt".format(config.project)
 try:
     with open(gource_input_txt, "w") as gource_file:
         gource_file.write("\n".join(gource_list))
+    print("Gource input is saved in '{0}'".format(gource_input_txt))
 except OSError:
     print("OSError while writing to file: " + gource_input_txt)
 except KeyboardInterrupt:
