96bc336de5c4d3bf13ea2981939511716238aefb
Andrei Rybak <rybak.a.v@gmail.com>
2018-07-15 19:58:54 +0200

    generate_gource.py: log time of writing file

--- generate_gource.py
+++ generate_gource.py
@@ -2,14 +2,19 @@ import jira
 from history_converter import convert_history
 import config
 
+from my_os import current_milli_time
+
 gource_list = convert_history(jira.changes[config.project],
                               config.sections_extension)
 
 gource_input_txt = "gource-input-{0}.txt".format(config.project)
 try:
+    start = current_milli_time()
     with open(gource_input_txt, "w", encoding='utf-8') as gource_file:
         gource_file.write("\n".join(gource_list))
     print("Gource input is saved in '{0}'".format(gource_input_txt))
+    finish = current_milli_time()
+    print("\tSaving took {} ms.".format(finish - start))
 except OSError:
     print("OSError while writing to file: " + gource_input_txt)
 except KeyboardInterrupt:
